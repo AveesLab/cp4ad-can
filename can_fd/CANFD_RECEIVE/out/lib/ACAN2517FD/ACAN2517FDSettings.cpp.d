57120a03e27b7414a895e48676de185277922171
hongkerous <132263191+hongkerous@users.noreply.github.com>
2025-02-06 14:20:53 +0900

    2025 버전 자료
    2025 버전 자료

---
+++ can_fd/CANFD_RECEIVE/out/lib/ACAN2517FD/ACAN2517FDSettings.cpp.d
@@ -0,0 +1,8 @@
+lib/ACAN2517FD/ACAN2517FDSettings.cpp.o: \
+ /cygdrive/c/Users/hongkerous2619/Desktop\진행중자료_자컴플/cp4ad-can-main/cp4ad-can-main/can_fd/CAN_basic/lib/ACAN2517FD/ACAN2517FDSettings.cpp \
+ ../lib/ACAN2517FD/ACAN2517FDSettings.h \
+ ../lib/ACAN2517FD/ACANFD_DataBitRateFactor.h
+
+../lib/ACAN2517FD/ACAN2517FDSettings.h:
+
+../lib/ACAN2517FD/ACANFD_DataBitRateFactor.h:
