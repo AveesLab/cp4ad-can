3f6b7061765ef8f598526d50155a91d688e63bff
hongkerous <132263191+hongkerous@users.noreply.github.com>
2025-02-06 17:08:53 +0900

    send 추가본(error: make에러)

--- can_fd/CANFD_RECEIVE/asw.c
+++ can_fd/CANFD_RECEIVE/asw.c
@@ -1,8 +1,6 @@
 #include "bsw.h"
-unsigned char len;
-unsigned long id;
-unsigned char buf[8];
 
+unsigned char len =16;
 
 
 ISR2(TimerISR)
@@ -14,19 +12,40 @@ ISR2(TimerISR)
 
 TASK(Task1) {
 
-	/*if(CAN_checkMsg() == CAN_MSGAVAIL)*/
-	if(CAN_checkMsg() == true) {
+	// if(CAN_checkMsg() == true) {
 		
-		printfSerial("--------------------------------------------------\n");
-		bool temp=CAN_readMsg(&id, &len, buf);
-		printfSerial("%d\n",temp);
-		printfSerial("Get data from ID : %x \n",id);
-		for (int i = 0; i < len; i++) {
-		    printfSerial("%x    ", buf[i]);
+	// 	printfSerial("--------------------------------------------------\n");
+	// 	bool temp=CAN_readMsg(&id, &len, buf);
+	// 	printfSerial("%d\n",temp);
+	// 	printfSerial("Get data from ID : %x \n",id);
+	// 	for (int i = 0; i < len; i++) {
+	// 	    printfSerial("%x    ", buf[i]);
 
-		}
-		printfSerial("\n");
+	// 	}
+	// 	printfSerial("\n");
+	// }
+	
+	can_fd_msg send_msg;
+	msg.id = 0x040;
+	msg.len = len;
+	unsigned char* buf_send=(unsigned char*)malloc(sizeof(unsigned char)*len);
+	for(unsigned char i=0; i<len; i++)
+	{
+		buf_send[i]=i;
 	}
+	memcpy(send_msg.buf,buf_send,len);
+	
+
+
+	printfSerial("--------------------------------------------------\n");
+	printfSerial("%d\n",CAN_sendMsg(send_msg););
+	printfSerial("[NM]: frame id: %16x length: %d send_message: ",send_msg.id,send_msg.len);
+	for(unsigned char i=0; i<send_msg.len;i++)
+	{
+		printfSerial(" %x",send_msg.buf[i]);
+	}
+	printfSerial("\n")
+
 
 
 	TerminateTask();
--- can_fd/CANFD_RECEIVE/erika/obj/ee_avr8_asm.S.d
+++
@@ -1,2 +0,0 @@
-obj/ee_avr8_asm.S.o: \
- /cygdrive/c/Users/HONGKE~1/Desktop\ม๘วเม฿~1/CP4AD-~1/CP4AD-~1/can_fd/CAN_BA~1/erika/src/ee_avr8_asm.S
